d8287dc73d627f32465a5dd8c6878e07596db9be
Debashish D. Dev <hello@debashish.dev>
2022-05-23 13:13:28 +0600

    Added DaisyUI Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#166534",
+          secondary: "#16a34a",
+          accent: "#14532d",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
