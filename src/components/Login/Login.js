e0c76816e2c0a5639a29dc70a147ceb3c6a38032
Debashish D. Dev <hello@debashish.dev>
2022-05-23 16:56:46 +0600

    Login Form Using React Form Hook Done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,16 @@ import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
 import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
   if (gLoading) {
     return <Loading></Loading>;
   }
@@ -15,12 +22,89 @@ const Login = () => {
   if (gUser) {
     console.log(gUser);
   }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="card w-96 bg-green-200 shadow-xl">
         <div className="card-body">
-          <h2 className="text-center text-3xl font-bold">Card title!</h2>
+          <h2 className="text-center text-3xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered w-full"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required! ",
+                  },
+                  pattern: {
+                    value:
+                      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "Enter Valid Email",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-600">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-600">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your Password"
+                className="input input-bordered w-full"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required! ",
+                  },
+                  minLength: {
+                    value: 8,
+                    message:
+                      "Password must be longer than 8 character or long!",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+
+            <input
+              className="btn btn-primary btn-outline w-full"
+              type="submit"
+              value="Login"
+            />
+          </form>
           <div className="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
