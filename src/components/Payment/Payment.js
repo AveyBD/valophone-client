230aeb3bebf1a3fd1cabcf9ba0353197dbee5991
Debashish D. Dev <hello@debashish.dev>
2022-05-24 23:28:16 +0600

    Loading order on payment Page

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Payment = () => {
-    const params = useParams();
-    const orderId = params.id;
-    return (
-        <div>
-            <p>{orderId}</p>
-        </div>
-    );
+  const params = useParams();
+  const [order, setOrder] = useState([]);
+  const orderId = params.id;
+  const url = `http://localhost:5000/orders/${orderId}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setOrder(data));
+  }, []);
+  console.log(order);
+  return (
+    <div>
+      <p>{orderId}</p>
+    </div>
+  );
 };
 
 export default Payment;
