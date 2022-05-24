281ddae2c72e4846f18cb16b71dc4a0e1b746077
Debashish D. Dev <hello@debashish.dev>
2022-05-24 12:15:11 +0600

    Added Dashboard Profiles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
+import MyOrder from "./components/Dashboard/MyOrder";
+import MyProfile from "./components/Dashboard/MyProfile";
+import MyReview from "./components/Dashboard/MyReview";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
@@ -23,7 +26,11 @@ function App() {
               <Dashboard />
             </RequireAuth>
           }
-        ></Route>
+        >
+          <Route index element={<MyOrder></MyOrder>}></Route>
+          <Route path="review" element={<MyReview></MyReview>}></Route>
+          <Route path="profile" element={<MyProfile></MyProfile>}></Route>
+        </Route>
       </Routes>
     </>
   );
