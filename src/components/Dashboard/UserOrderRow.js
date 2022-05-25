3799ae14cdcddea73933fa6544b20342d2c4a6a5
Debashish D. Dev <hello@debashish.dev>
2022-05-25 12:49:38 +0600

    removed unused dependancy

--- src/components/Dashboard/UserOrderRow.js
+++ src/components/Dashboard/UserOrderRow.js
@@ -1,4 +1,4 @@
-import { faDollar, faDollarSign, faMoneyBillAlt, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyBillAlt, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
