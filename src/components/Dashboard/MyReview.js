8f95f17b95a4b6c5e95d2c56c02f6873ea354db0
Debashish D. Dev <hello@debashish.dev>
2022-05-25 17:59:35 +0600

    Review Page

--- src/components/Dashboard/MyReview.js
+++ src/components/Dashboard/MyReview.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const MyReview = () => {
-    return (
+  return (
+    <div>
+      <h2>My Review</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-3/4 mx-auto">
         <div>
-            <h2>My Review</h2>
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">Previous Reviews</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+        <div>
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">Add New Review</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyReview;
