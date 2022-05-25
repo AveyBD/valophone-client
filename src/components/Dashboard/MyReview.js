cc2240ea36830e757b34bf3823126bf63f5bdbd9
Debashish D. Dev <hello@debashish.dev>
2022-05-25 18:18:50 +0600

    Add form user review

--- src/components/Dashboard/MyReview.js
+++ src/components/Dashboard/MyReview.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const MyReview = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <h2>My Review</h2>
@@ -20,10 +29,80 @@ const MyReview = () => {
           <div class="card w-96 bg-base-100 shadow-xl">
             <div class="card-body">
               <h2 class="card-title">Add New Review</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
-              </div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Your Email"
+                    className="input input-bordered w-full"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Email is required! ",
+                      },
+                      pattern: {
+                        value:
+                          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: "Enter Valid Email",
+                      },
+                    })}
+                  />
+                  <label className="label">
+                    {errors.email?.type === "required" && (
+                      <span className="label-text-alt text-red-600">
+                        {errors.email.message}
+                      </span>
+                    )}
+                    {errors.email?.type === "pattern" && (
+                      <span className="label-text-alt text-red-600">
+                        {errors.email.message}
+                      </span>
+                    )}
+                  </label>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Your Password"
+                    className="input input-bordered w-full"
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "Password is required! ",
+                      },
+                      minLength: {
+                        value: 8,
+                        message:
+                          "Password must be longer than 8 character or long!",
+                      },
+                    })}
+                  />
+                  <label className="label">
+                    {errors.password?.type === "required" && (
+                      <span className="label-text-alt text-red-600">
+                        {errors.password.message}
+                      </span>
+                    )}
+                    {errors.password?.type === "minLength" && (
+                      <span className="label-text-alt text-red-600">
+                        {errors.password.message}
+                      </span>
+                    )}
+                  </label>
+                </div>
+
+                <input
+                  className="btn btn-primary btn-outline w-full"
+                  type="submit"
+                  value="Login"
+                />
+              </form>
             </div>
           </div>
         </div>
