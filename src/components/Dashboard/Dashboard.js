b75e4426ec2c9bf506a5d8ea7f625b7b7be2b245
Debashish D. Dev <hello@debashish.dev>
2022-05-25 23:42:01 +0600

    Conditional Sidebar Menu Done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [loggedUser, setLoggedUser] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/user/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setLoggedUser(data));
+  }, []);
   return (
     <div className="drawer drawer-mobile">
       <input id="dashDrawer" type="checkbox" className="drawer-toggle" />
@@ -12,28 +21,35 @@ const Dashboard = () => {
       <div className="drawer-side">
         <label htmlFor="dashDrawer" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-          {/* <!-- Sidebar content here --> */}
-          <li>
-            <Link to={"/dashboard"}>My Order</Link>
-          </li>
-          <li>
-            <Link to={"/dashboard/review"}>My Review</Link>
-          </li>
+          {loggedUser.role !== "admin" && (
+            <>
+              <li>
+                <Link to={"/dashboard"}>My Order</Link>
+              </li>
+              <li>
+                <Link to={"/dashboard/review"}>My Review</Link>
+              </li>
+            </>
+          )}
           <li>
             <Link to={"/dashboard/profile"}>My Profile</Link>
           </li>
-          <li>
-            <Link to={"/dashboard/manageproduct"}>Manage Product</Link>
-          </li>
-          <li>
-            <Link to={"/dashboard/manage"}>Manage Order</Link>
-          </li>
-          <li>
-            <Link to={"/dashboard/users"}>Manage Users</Link>
-          </li>
-          <li>
-            <Link to={"/dashboard/addproduct"}>Add Product</Link>
-          </li>
+          {loggedUser.role === "admin" && (
+            <>
+              <li>
+                <Link to={"/dashboard/manageproduct"}>Manage Product</Link>
+              </li>
+              <li>
+                <Link to={"/dashboard/manage"}>Manage Order</Link>
+              </li>
+              <li>
+                <Link to={"/dashboard/users"}>Manage Users</Link>
+              </li>
+              <li>
+                <Link to={"/dashboard/addproduct"}>Add Product</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
