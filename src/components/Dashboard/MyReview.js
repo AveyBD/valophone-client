0b36ec29176eec20ef9b38b549a64ad4f2a459f9
Debashish D. Dev <hello@debashish.dev>
2022-05-26 15:35:19 +0600

    My Review Working

--- src/components/Dashboard/MyReview.js
+++ src/components/Dashboard/MyReview.js
@@ -46,10 +46,7 @@ const MyReview = () => {
           <div class="card w-96 bg-base-100 shadow-xl">
             <div class="card-body">
               <h2 class="card-title">Previous Reviews</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
-              </div>
+              <p>The function has not Yet Implemented!</p>
             </div>
           </div>
         </div>
