5791a4166829d2d0701d6696259a3925c18fb444
Debashish D. Dev <hello@debashish.dev>
2022-05-26 17:47:08 +0600

    Updated Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Hero from "./Hero";
 import LatestParts from "./LatestParts";
+import Newsletter from "./Newsletter";
+import Partner from "./Partner";
+import Shipment from "./Shipment";
+import Stats from "./Stats";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestParts></LatestParts>
+      <Stats></Stats>
+      <Shipment></Shipment>
+      <Newsletter></Newsletter>
+      <Partner></Partner>
     </div>
   );
 };
